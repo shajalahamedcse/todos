93bdf872c31043cf4fdaf935b0de1f9e6ecf2faf
shajalAhamed <shajalahamedcse@gmail.com>
2020-10-31 19:58:57 +0600

    Added readme file

--- src/resource/user.py
+++ src/resource/user.py
@@ -2,3 +2,11 @@ from flask import Blueprint
 from src.utils import ResponseGenerator
 
 user_blueprint = Blueprint('user', __name__)
+
+@user_blueprint.route("/api/v1/register")
+def register():
+    return ResponseGenerator.generate_response("Hell0", 200)
+
+@user_blueprint.route("/api/v1/login")
+def login():
+    return ResponseGenerator.generate_response("Hell0", 200)
