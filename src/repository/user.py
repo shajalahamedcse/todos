5ddc64b6f4123aa0dcbb5d2e34c7cdfb253e483b
shajalAhamed <shajalahamedcse@gmail.com>
2020-10-31 18:18:27 +0600

    Added user repository

---
+++ src/repository/user.py
@@ -0,0 +1,28 @@
+from typing import List, Dict, Union, Optional
+from src.models.user import User
+
+class UserRepository:
+    
+    @staticmethod
+    def create_user(name: str, email: str, password: str, **kwargs) -> Optional[User]:
+        existing_user = UserRepository.get_by_email(email)
+        if existing_user:
+            return None
+
+        new_user = User(name, email, password, **kwargs)
+        return new_user.save()
+    
+    @staticmethod
+    def get_all() -> List[User]:
+        return User.query.all()
+    
+    @staticmethod
+    def get_by_id(id: int) -> User:
+        # TODO: implement mechanism to get user form cache
+        return User.query.get(id)
+
+    @staticmethod
+    def get_by_email(email: str) -> User:
+        return User.query.filter(
+            User.email == email
+        ).first()
